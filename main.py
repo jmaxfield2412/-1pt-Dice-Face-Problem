116de91d96b5335f40cb2d237c2e456f20c62a79
jmaxfield2412 <jmaxfield@student.strode-college.ac.uk>
2020-11-04 09:43:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Dice Face 5 Problem
+
+#Main Program
+
+def Output5():
+  print("00000000000")
+  print("0         0")
+  print("0  #   #  0")
+  print("0    #    0")
+  print("0  #   #  0")
+  print("0         0")
+  print("00000000000")
+#Subroutine to output text
+Output5()
